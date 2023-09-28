4464c57dc2aeec4c8e1a8a9d749c21aba6091ef5
Abu Desai <abhyuday.desai@gmail.com>
2023-09-27 22:08:38 -0700

    first commit

---
+++ src/predict.r
@@ -0,0 +1,132 @@
+# Required Libraries
+library(tidyverse)
+library(jsonlite)
+library(fastDummies)
+library(e1071)
+
+# Paths
+ROOT_DIR <- dirname(getwd())
+MODEL_INPUTS_OUTPUTS <- file.path(ROOT_DIR, 'model_inputs_outputs')
+INPUT_DIR <- file.path(MODEL_INPUTS_OUTPUTS, "inputs")
+OUTPUT_DIR <- file.path(MODEL_INPUTS_OUTPUTS, "outputs")
+INPUT_SCHEMA_DIR <- file.path(INPUT_DIR, "schema")
+DATA_DIR <- file.path(INPUT_DIR, "data")
+TRAIN_DIR <- file.path(DATA_DIR, "training")
+TEST_DIR <- file.path(DATA_DIR, "testing")
+MODEL_PATH <- file.path(MODEL_INPUTS_OUTPUTS, "model")
+MODEL_ARTIFACTS_PATH <- file.path(MODEL_PATH, "artifacts")
+OHE_ENCODER_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'ohe.rds')
+PREDICTOR_DIR_PATH <- file.path(MODEL_ARTIFACTS_PATH, "predictor")
+PREDICTOR_FILE_PATH <- file.path(PREDICTOR_DIR_PATH, "predictor.rds")
+IMPUTATION_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'imputation.rds')
+PREDICTIONS_DIR <- file.path(OUTPUT_DIR, 'predictions')
+PREDICTIONS_FILE <- file.path(PREDICTIONS_DIR, 'predictions.csv')
+TOP_10_CATEGORIES_MAP <- file.path(MODEL_ARTIFACTS_PATH, "top_10_map.rds")
+COLNAME_MAPPING <- file.path(MODEL_ARTIFACTS_PATH, "colname_mapping.csv")
+SCALING_FILE <- file.path(MODEL_ARTIFACTS_PATH, "scaler.rds")
+
+
+if (!dir.exists(PREDICTIONS_DIR)) {
+  dir.create(PREDICTIONS_DIR, recursive = TRUE)
+}
+
+# Reading the schema
+file_name <- list.files(INPUT_SCHEMA_DIR, pattern = "*.json")[1]
+schema <- fromJSON(file.path(INPUT_SCHEMA_DIR, file_name))
+features <- schema$features
+
+numeric_features <- features$name[features$dataType != 'CATEGORICAL']
+categorical_features <- features$name[features$dataType == 'CATEGORICAL']
+id_feature <- schema$id$name
+target_feature <- schema$target$name
+target_classes <- schema$target$classes
+model_category <- schema$modelCategory
+nullable_features <- features$name[features$nullable == TRUE]
+
+
+# Reading test data.
+file_name <- list.files(TEST_DIR, pattern = "*.csv", full.names = TRUE)[1]
+# Read the first line to get column names
+header_line <- readLines(file_name, n = 1)
+col_names <- unlist(strsplit(header_line, split = ",")) # assuming ',' is the delimiter
+# Read the CSV with the exact column names
+df <- read.csv(file_name, skip = 0, col.names = col_names, check.names=FALSE)
+
+
+# Data preprocessing
+# Note that when we work with testing data, we have to impute using the same values learned during training. This is to avoid data leakage.
+imputation_values <- readRDS(IMPUTATION_FILE)
+
+for (column in nullable_features) {
+    # Create missing indicator
+    missing_indicator_col_name <- paste(column, "is_missing", sep="_")
+    df[[missing_indicator_col_name]] <- ifelse(is.na(df[[column]]), 1, 0)
+    
+    # Impute missing values
+    if (!is.null(imputation_values[[column]])) {
+        df[, column][is.na(df[, column])] <- imputation_values[[column]]
+    }
+}
+
+# Saving the id column in a different variable and then dropping it.
+ids <- df[[id_feature]]
+df[[id_feature]] <- NULL
+
+# Encoding
+# We encode the data using the same encoder that we saved during training.
+if (length(categorical_features) > 0 && file.exists(OHE_ENCODER_FILE)) {
+  top_10_map <- readRDS(TOP_10_CATEGORIES_MAP)
+  encoder <- readRDS(OHE_ENCODER_FILE)
+  for(col in categorical_features) {
+    # Use the saved top 10 categories to replace values outside these categories with 'Other'
+    df[[col]][!(df[[col]] %in% top_10_map[[col]])] <- "Other"
+  }
+
+  test_df_encoded <- dummy_cols(df, select_columns = categorical_features, remove_selected_columns = TRUE)
+  encoded_columns <- readRDS(OHE_ENCODER_FILE)
+  # Add missing columns with 0s
+    for (col in encoded_columns) {
+        if (!col %in% colnames(test_df_encoded)) {
+            test_df_encoded[[col]] <- 0
+        }
+    }
+
+# Remove extra columns
+    extra_cols <- setdiff(colnames(test_df_encoded), c(colnames(df), encoded_columns))
+    df <- test_df_encoded[, !names(test_df_encoded) %in% extra_cols]
+}
+
+# Standard Scaling
+scaling_values <- readRDS(SCALING_FILE) # Assuming you've saved scaling values during training
+for (feature in numeric_features) {
+    df[[feature]] <- (df[[feature]] - scaling_values[[feature]]$mean) / scaling_values[[feature]]$std
+}
+
+# Outlier Capping for Standard Scaled Data
+lower_bound <- -4
+upper_bound <- 4
+
+for (feature in numeric_features) {
+    df[[feature]] <- ifelse(df[[feature]] < lower_bound, lower_bound, df[[feature]])
+    df[[feature]] <- ifelse(df[[feature]] > upper_bound, upper_bound, df[[feature]])
+}
+
+# Load the column name mapping
+colname_mapping <- read.csv(COLNAME_MAPPING)
+# Update the column names based on the mapping
+df <- df[, colname_mapping$original]
+colnames(df) <- colname_mapping$sanitized[match(colnames(df), colname_mapping$original)]
+
+
+# Making predictions
+model <- readRDS(PREDICTOR_FILE_PATH)
+
+# Predict using the SVR model
+predictions <- predict(model, newdata = df)
+
+# Creating predictions DataFrame
+predictions_df <- data.frame(prediction = predictions)
+predictions_df <- tibble::tibble(ids = ids) %>% dplyr::bind_cols(predictions_df)
+colnames(predictions_df)[1] <- id_feature
+
+write.csv(predictions_df, PREDICTIONS_FILE, row.names = FALSE)
---
+++ src/train.r
@@ -0,0 +1,196 @@
+# Required Libraries
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(fastDummies)
+library(e1071)
+
+# Define directories and paths
+
+ROOT_DIR <- dirname(getwd())
+MODEL_INPUTS_OUTPUTS <- file.path(ROOT_DIR, 'model_inputs_outputs')
+INPUT_DIR <- file.path(MODEL_INPUTS_OUTPUTS, "inputs")
+INPUT_SCHEMA_DIR <- file.path(INPUT_DIR, "schema")
+DATA_DIR <- file.path(INPUT_DIR, "data")
+TRAIN_DIR <- file.path(DATA_DIR, "training")
+MODEL_ARTIFACTS_PATH <- file.path(MODEL_INPUTS_OUTPUTS, "model", "artifacts")
+IMPUTATION_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'imputation.rds')
+TOP_10_CATEGORIES_MAP <- file.path(MODEL_ARTIFACTS_PATH, "top_10_map.rds")
+COLNAME_MAPPING <- file.path(MODEL_ARTIFACTS_PATH, "colname_mapping.csv")
+SCALING_FILE <- file.path(MODEL_ARTIFACTS_PATH, "scaler.rds")
+OHE_ENCODER_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'ohe.rds')
+PREDICTOR_FILE_PATH <- file.path(MODEL_ARTIFACTS_PATH, "predictor", "predictor.rds")
+
+
+if (!dir.exists(MODEL_ARTIFACTS_PATH)) {
+    dir.create(MODEL_ARTIFACTS_PATH, recursive = TRUE)
+}
+if (!dir.exists(file.path(MODEL_ARTIFACTS_PATH, "predictor"))) {
+    dir.create(file.path(MODEL_ARTIFACTS_PATH, "predictor"))
+}
+
+
+# Reading the schema
+# The schema contains metadata about the datasets. 
+# We will use the scehma to get information about the type of each feature (NUMERIC or CATEGORICAL)
+# and the id and target features, this will be helpful in preprocessing stage.
+
+file_name <- list.files(INPUT_SCHEMA_DIR, pattern = "*.json")[1]
+schema <- fromJSON(file.path(INPUT_SCHEMA_DIR, file_name))
+features <- schema$features
+
+numeric_features <- features$name[features$dataType == "NUMERIC"]
+categorical_features <- features$name[features$dataType == "CATEGORICAL"]
+id_feature <- schema$id$name
+target_feature <- schema$target$name
+model_category <- schema$modelCategory
+nullable_features <- features$name[features$nullable == TRUE]
+
+# Reading training data
+file_name <- list.files(TRAIN_DIR, pattern = "*.csv")[1]
+# Read the first line to get column names
+header_line <- readLines(file.path(TRAIN_DIR, file_name), n = 1)
+col_names <- unlist(strsplit(header_line, split = ",")) # assuming ',' is the delimiter
+# Read the CSV with the exact column names
+df <- read.csv(file.path(TRAIN_DIR, file_name), skip = 0, col.names = col_names, check.names=FALSE)
+
+# Data Preprocessing
+# Impute missing data
+imputation_values <- list()
+
+for (column in nullable_features) {
+    # Create missing indicator
+    missing_indicator_col_name <- paste(column, "is_missing", sep="_")
+    df[[missing_indicator_col_name]] <- ifelse(is.na(df[[column]]), 1, 0)
+    
+    if (column %in% numeric_features) {
+        value <- median(df[, column], na.rm = TRUE)
+    } else {
+        value <- as.character(df[, column] %>% tidyr::replace_na())
+        value <- value[1]
+    }
+    df[, column][is.na(df[, column])] <- value
+    imputation_values[column] <- value
+}
+saveRDS(imputation_values, IMPUTATION_FILE)
+
+
+# Encoding Categorical features
+
+# The id column is just an identifier for the training example, so we will exclude it during the encoding phase.
+# Target feature will be label encoded in the next step.
+
+ids <- df[, id_feature]
+target <- df[, target_feature]
+df <- df %>% select(-all_of(c(id_feature, target_feature)))
+
+
+# One Hot Encoding
+if(length(categorical_features) > 0){
+    top_10_map <- list()
+    for(col in categorical_features) {
+        # Get the top 10 categories for the column
+        top_10_categories <- names(sort(table(df[[col]]), decreasing = TRUE)[1:10])
+
+        # Save the top 3 categories for this column
+        top_10_map[[col]] <- top_10_categories
+        # Replace categories outside the top 3 with "Other"
+        df[[col]][!(df[[col]] %in% top_10_categories)] <- "Other"
+    }
+
+    df_encoded <- dummy_cols(df, select_columns = categorical_features, remove_selected_columns = TRUE)
+    encoded_columns <- setdiff(colnames(df_encoded), colnames(df))
+    saveRDS(encoded_columns, OHE_ENCODER_FILE)
+    saveRDS(top_10_map, TOP_10_CATEGORIES_MAP)
+    df <- df_encoded
+}
+
+# Standard Scaling
+scaling_values <- list()
+for (feature in numeric_features) {
+    feature_mean <- mean(df[[feature]], na.rm = TRUE)
+    feature_std <- sd(df[[feature]], na.rm = TRUE)
+    scaling_values[[feature]] <- list(mean = feature_mean, std = feature_std)
+    
+    # Standardize the feature
+    df[[feature]] <- (df[[feature]] - feature_mean) / feature_std
+}
+
+# Save the scaling values for use during testing
+saveRDS(scaling_values, SCALING_FILE)
+
+# Cap outliers
+lower_bound <- -4
+upper_bound <- 4
+for (feature in numeric_features) {
+    df[[feature]] <- ifelse(df[[feature]] < lower_bound, lower_bound, df[[feature]])
+    df[[feature]] <- ifelse(df[[feature]] > upper_bound, upper_bound, df[[feature]])
+}
+
+
+# Sanitize column names with special characters or spaces
+sanitize_colnames <- function(names_vector) {
+  # Check for unique original column names
+  if (any(duplicated(names_vector))) {
+    stop("Error: Given column names are not unique!")
+  }
+
+  # Trim spaces from column names
+  names_vector <- trimws(names_vector)
+
+  # Special characters sanitization
+  sanitized_names <- gsub(" ", "_", names_vector)
+  sanitized_names <- gsub("[^[:alnum:]_]", "_", sanitized_names)
+
+  # Prefix with "feat_" - this is to get around columns that start with numbers
+  sanitized_names <- paste0("feat_", sanitized_names)
+  
+  # Ensure uniqueness
+  while(any(duplicated(sanitized_names))) {
+    dupes <- table(sanitized_names)
+    dupes <- as.character(names(dupes[dupes > 1]))
+    
+    for(d in dupes) {
+      indices <- which(sanitized_names == d)
+      sanitized_names[indices] <- paste0(d, "_", seq_along(indices))
+    }
+  }  
+  return(sanitized_names)
+}
+
+# save the column name mapping to a file
+new_colnames <- sanitize_colnames(colnames(df))
+colname_mapping <- data.frame(
+  original = colnames(df),
+  sanitized = new_colnames
+)
+write.csv(colname_mapping, COLNAME_MAPPING, row.names = FALSE)
+
+# apply new column names to df
+colnames(df) <- new_colnames
+
+# SVR Training
+
+df[[target_feature]] <- target  # Add the target feature back to the dataframe
+
+
+# Set SVR parameters
+epsilon <- 0.1
+cost <- 1
+gamma <- 0.0  
+kernel <- "radial"  # This is the RBF kernel, you can change to "linear", "polynomial" etc. based on your needs
+
+# Train the model using SVR
+model <- svm(
+  as.formula(paste(target_feature, "~ .")), 
+  data=df, 
+  method="C-classification", 
+  kernel=kernel, 
+  cost=cost, 
+  epsilon=epsilon,
+  gamma=gamma
+)
+
+# Save the model
+saveRDS(model, PREDICTOR_FILE_PATH)
